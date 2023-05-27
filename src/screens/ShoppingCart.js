f68611b1efd38fabd801ee8eadac462eb1b00317
Konrad Rydygier <konradr97@gmail.com>
2023-05-27 13:38:02 +0200

    Add Shopping Cart

---
+++ src/screens/ShoppingCart.js
@@ -0,0 +1,93 @@
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
+import cart from "../data/cart";
+import CartListItem from "../components/CartListItem";
+
+const ShoppingCartTotals = () => {
+  return (
+    <View style={styles.totalsContainer}>
+      <View style={styles.row}>
+        <Text style={styles.text}>Subtotal</Text>
+        <Text style={styles.text}>410,00 US$</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.text}>Delivery</Text>
+        <Text style={styles.text}>16,50 US$</Text>
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.textBold}>Total</Text>
+        <Text style={styles.textBold}>426,50 US$</Text>
+      </View>
+    </View>
+  );
+};
+
+const ShoppingCart = () => {
+  return (
+    <>
+      <FlatList
+        data={cart}
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+        ListFooterComponent={() => <ShoppingCartTotals />}
+      />
+      <View style={styles.footer}>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Checkout</Text>
+        </Pressable>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  totalsContainer: {
+    margin: 20,
+    paddingTop: 10,
+    borderColor: "gainsboro",
+    borderTopWidth: 1,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 2,
+  },
+  text: {
+    fontSize: 16,
+    color: "gray",
+  },
+  textBold: {
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  itemContainer: {
+    width: "50%",
+    padding: 1,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  footer: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    backgroundColor: "white",
+    borderColor: "gainsboro",
+    borderTopWidth: 1,
+    padding: 20,
+  },
+  button: {
+    width: "100%",
+    backgroundColor: "black",
+    alignSelf: "center",
+    alignItems: "center",
+    padding: 20,
+    borderRadius: 100,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 16,
+  },
+});
+
+export default ShoppingCart;
